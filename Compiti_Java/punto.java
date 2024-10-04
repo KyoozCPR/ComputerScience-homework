a72154bec524d6070880bec67c1b0be5deeee796
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2024-10-04 12:30:30 +0200

    Create punto.java

---
+++ Compiti_Java/punto.java
@@ -0,0 +1,27 @@
+package progetto_iniziale;
+
+public class Punto {
+	//Definisco gli attributi della classe
+	private double x,y;
+	private int quadrante;
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	// this rappresenta l'oggetto
+	
+	public void setX(double pippo) {
+		this.x=pippo;
+	}
+	
+	public void setY(double pippo) {
+		this.y=pippo;
+	}
+	
+	
+}
