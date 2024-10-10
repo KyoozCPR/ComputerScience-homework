93b204d25c83ebd2aaf1c334fcb5b20119752eab
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2024-10-10 10:37:26 +0200

    finito funzionalità del calcolo quadrante

--- Compiti_Java/punto.java
+++ Compiti_Java/punto.java
@@ -1,9 +1,7 @@
-package progetto_iniziale;
 
 public class Punto {
-	//Definisco gli attributi della classe
-	private double x,y;
-	private int q;
+	
+	public int q, x,y;
 	
 	public double getX() {
 		return x;
@@ -15,15 +13,15 @@ public class Punto {
 	
 	// this rappresenta l'oggetto
 	
-	public void setX(double pippo) {
+	public void setX(int pippo) {
 		this.x=pippo;
 	}
 	
-	public void setY(double pippo) {
+	public void setY(int pippo) {
 		this.y=pippo;
 	}
 	
-	public Punto(double x, double y) {
+	public Punto(int x, int y) {
 		setX(x);
 		setY(y);
 		setQ();
@@ -32,5 +30,13 @@ public class Punto {
 	private void setQ() {
 		if (x >= 0 && y >= 0)
 			this.q=1;
+		else if(x < 0 && y>0)
+            this.q=2;
+        else if(x < 0 && y<0)
+            this.q=3;
+        else if(x > 0 && y<0)
+            this.q=4;
+        else 
+            this.q=0;
 	}
 }
