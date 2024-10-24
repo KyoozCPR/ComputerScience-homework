97039302a352d286f75a4cdbb8ef00e1ee9a80a8
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2024-10-24 10:23:57 +0200

    Creata classe rubrica per il main

--- Compiti_Java/Compito_Rubrica/Rubrica.java
+++ Compiti_Java/Compito_Rubrica/Rubrica.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+
+public class Rubrica {
+	Scanner tastiera = new Scanner(System.in);
+	
+	public Contatto input() {
+		String nome, cognome, telefono;
+		int giorno,mese,anno;
+		Data data;
+		nome = tastiera.nextLine();
+		cognome = tastiera.nextLine();
+		telefono = tastiera.nextLine();
+		giorno = tastiera.nextInt();
+		mese = tastiera.nextInt();
+		anno= tastiera.nextInt();
+		Data dataNascita = new Data(giorno, mese, anno);
+		Contatto persona = new Contatto(nome, cognome, telefono, dataNascita);
+		return persona;
+	}
+
+	public static void main(String[] args) {
+		Rubrica rubrica = new Rubrica();
+		Contatto persona = rubrica.input();
+		
+	}
+
+}
 
