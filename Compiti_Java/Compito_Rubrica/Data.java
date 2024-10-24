96d67fca2bffeecae844f127b89d7b4e729da902
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2024-10-24 09:52:42 +0200

    Creato classe data

--- Compiti_Java/Compito_Rubrica/Data.java
+++ Compiti_Java/Compito_Rubrica/Data.java
@@ -1,1 +1,39 @@
 
+public class Data {
+	private int gg, mm, aaaa;
+	
+	public int getGg() {
+		return this.gg;
+	}
+	public int getMm(){
+		return this.mm;
+	}
+	public int getAaaa() {
+		return this.aaaa;
+	}
+	
+	public void setGg(int giorno) {
+		this.gg = giorno;
+	}
+	public void setMm(int giorno) {
+		this.gg = giorno;
+	}
+	public void setAaaa(int giorno) {
+		this.gg = giorno;
+	}
+	
+	
+	public Data(int giorno, int mese, int anno) {
+		setGg(giorno);
+		setMm(mese);
+		setAaaa(anno);
+	}
+	
+	@Override
+	public String toString() {
+		return "La data è: " + this.gg + "/" + this.mm + "/" + this.aaaa;
+	}
+	
+	
+	
+}
