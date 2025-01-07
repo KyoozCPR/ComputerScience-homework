204c8b9a182e609008737ce5a7e6b2f365054328
Diego Computer <cipriettid949@gmail.com>
2025-01-07 22:30:28 +0100

    nuovo compito

--- Compiti_Java/Compito_Edificio/Edificio.java
+++ Compiti_Java/Compito_Edificio/Edificio.java
@@ -16,30 +16,67 @@ public class Edificio {
 
     public int input(){
         System.out.println(
-                "0) Uscire" +
-                        "Aggiungere un utente e verificarne il login.\n" +
-                        "Prenotare una sala disponibile.\n" +
-                        "Visualizzare l’elenco delle sale e il loro stato (disponibile o occupata).\n" +
-                        "Liberare una sala occupata.\n" +
-                        "Cercare le sale che hanno una capacità minima specificata.\n" +
-                        "Ordinare le sale per capacità."
+                "0) Uscire\n" +
+                        "1) Aggiungere un utente e verificarne il login.\n" +
+                        "2) Prenotare una sala disponibile.\n" +
+                        "3) Visualizzare l’elenco delle sale e il loro stato (disponibile o occupata).\n" +
+                        "4) Liberare una sala occupata.\n" +
+                        "5) Cercare le sale che hanno una capacità minima specificata.\n" +
+                        "6) Ordinare le sale per capacità."
 
         );
         return tastiera.nextInt();
     }
 
+    private Utente login(String email, String pwd){
+        for (Utente utente : utenti) {
+            if (utente.getEmail().equals(email) && utente.getPassword().equals(pwd)) {
+                return utente;
+            }
+        }
+        return null;
+    }
 
     public void menu(int scelta){
+        String nome, email, pwd;
         switch (scelta){
-            case 0: System.out.println("Arrivederci!");
+            case 0: System.out.println("Arrivederci!"); break;
             case 1:
                 System.out.println("\nInserire il nome dell'utente: ");
-                String nome = tastiera.nextLine();
+                nome = tastiera.nextLine();
                 System.out.println("Inserire l'email: ");
-                String email = tastiera.nextLine();
+                email = tastiera.nextLine();
                 System.out.println("Inserisci una password: ");
-                String pwd = tastiera.nextLine();
+                pwd = tastiera.nextLine();
+
                 utenti.add(new Utente(nome, email, pwd));
+                break;
+
+            case 2:
+                System.out.println("Scegli quale sala da prenotare tra le elencate: ");
+                for (int i=0; i< sala.size(); i++){
+                    System.out.println("\t"+ i+") " + sala.get(i).getNome());
+                }
+                int scelta_sala = tastiera.nextInt();
+                System.out.println("Effettuare il login ad un account per prenotare: ");
+                tastiera.nextLine().trim();
+                System.out.println("Inserire l'email: ");
+                email = tastiera.nextLine();
+                System.out.println("Inserisci una password: ");
+                pwd = tastiera.nextLine();
+                Utente logged = login(email, pwd);
+                if (logged != null){
+                    logged.setSala_prenotata(sala.get(scelta_sala));
+                }
+                else {
+                    System.out.println("Credenziali non valide!");
+                }
+                break;
+
+            case 3:
+
+                break;
+
 
             default:
                 System.out.println("Scelta non valida, riprovare!");
@@ -52,6 +89,7 @@ public class Edificio {
 
         do {
             scelta = obj.input();
+            obj.tastiera.nextLine().trim();
             obj.menu(scelta);
         } while(scelta != 0);
     }
--- Compiti_Java/Compito_Edificio/Utente.java
+++ Compiti_Java/Compito_Edificio/Utente.java
@@ -5,12 +5,17 @@ public class Utente {
     private String email;
     private String password;
     private Sala sala_prenotata;
+    private boolean login;
     public String getNome(){return nome;}
     public String getEmail(){return email;}
     public String getPassword(){return password;}
 
     public Sala getSala_prenotata() {
         return sala_prenotata;
+    }
+
+    public boolean isLogin() {
+        return login;
     }
 
     public void setNome(String nome) {
@@ -25,11 +30,21 @@ public class Utente {
         this.password = isValidPassword(password);
     }
 
+    public void setLogin(boolean login) {
+        this.login = login;
+    }
+
+    public void setSala_prenotata(Sala sala){
+        this.sala_prenotata = sala;
+    }
+
     public Utente(String nome, String email, String pwd){
         setNome(nome);
         setEmail(email);
         setPassword(pwd);
         this.sala_prenotata = null;
+        this.login = false;
+
     }
 
 
@@ -41,14 +56,16 @@ public class Utente {
                Può essere interpretato come una condizione
              */
             return password;
+        System.out.println("Password non valida! Assegnata una di default\n");
         return "defaultP$_";
 
     }
 
     private String isValidEmail(String email){
         //example@dominio.com
-        if (email.matches("(.+)@([a-z]+)\\.(com || it)"))
+        if (email.matches("(.+)@([a-z]+)\\.(com|it)"))
             return email;
+        System.out.println("Email non valida! Assegnata una di default");
         return "example@dominio.com";
     }
 
