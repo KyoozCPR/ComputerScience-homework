daef0c62d35404a10d3b1cd54e491c8c53832716
KyoozCPR <cipriettid949@gmail.com>
2025-01-12 18:44:19 +0100

    implementata la classe Compagnia

--- Compiti_Java/Compito_Noleggio/Compagnia.java
+++ Compiti_Java/Compito_Noleggio/Compagnia.java
@@ -1,4 +1,42 @@
 package progetto_iniziale.Compito_Noleggio;
-
+import java.util.ArrayList;
 public class Compagnia {
+    private String nome;
+    private String nomeTitolare;
+    private String cognomeTitolare;
+    private ArrayList<Veicolo> veicoli;
+
+    public String getNome(){return nome;}
+    public String getNomeTitolare(){return nomeTitolare;}
+    public String getCognomeTitolare(){return cognomeTitolare;}
+    public ArrayList<Veicolo> getVeicoli(){return veicoli;}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNomeTitolare(String nomeTitolare) {
+        this.nomeTitolare = nomeTitolare;
+    }
+
+    public void setCognomeTitolare(String cognomeTitolare) {
+        this.cognomeTitolare = cognomeTitolare;
+    }
+
+
+
+    public void setVeicoli(ArrayList<Veicolo> veicoli) {
+        this.veicoli = veicoli;
+    }
+
+    public Compagnia(){
+
+    }
+
+    public Compagnia(String nome, String nomeTitolare, String cognomeTitolare, Veicolo[] veicoli){
+
+
+    }
+
+
 }
