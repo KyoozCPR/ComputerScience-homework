6c351edc2d27f25784e7ce1c0cac9158ca9d86f7
KyoozCPR <cipriettid949@gmail.com>
2025-01-13 14:01:07 +0100

    finito

--- Compiti-TPS/Orologio/oraAttuale/script.js
+++ Compiti-TPS/Orologio/oraAttuale/script.js
@@ -1,12 +1,15 @@
+let line = 0;
 function updateDateTime() {
     const now = new Date();
 
     const date =now.toLocaleDateString();
     const time = now.getHours() + ":" + now.getMinutes()+":"+now.getSeconds();
 
-
+    line += 6;
     document.getElementById('date').textContent = date;
     document.getElementById('time').textContent = time;
+    document.getElementById('line').style =
+        'rotate:' +line+'deg';
 }
 
 setInterval(updateDateTime, 1000);
