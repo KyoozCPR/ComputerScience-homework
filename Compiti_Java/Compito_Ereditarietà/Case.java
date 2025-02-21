5bdb369a7e49bd24e393879370d3c50924525abf
KyoozCPR <cipriettid949@gmail.com>
2025-02-21 12:47:20 +0100

    Aggiunto compito iniziale sull'ereditarietà

--- Compiti_Java/Compito_Ereditarietà/Case.java
+++ Compiti_Java/Compito_Ereditarietà/Case.java
@@ -1,7 +1,48 @@
 package progetto_iniziale.Compito_Ereditarietà;
 
 public class Case extends Prodotto{
-    public Case(String nome, float prezzo, String CodiceBarre) {
+    private int ram;
+    private String os;
+    private int ssd;
+    private String motherboard;
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public int getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(int ssd) {
+        this.ssd = ssd;
+    }
+
+    public String getMotherboard() {
+        return motherboard;
+    }
+
+    public void setMotherboard(String motherboard) {
+        this.motherboard = motherboard;
+    }
+
+    public Case(String nome, float prezzo, String CodiceBarre, int ram, String os, int ssd, String motherboard) {
         super(nome, prezzo, CodiceBarre);
+        setMotherboard(motherboard);
+        setOs(os);
+        setRam(ram);
+
     }
 }
