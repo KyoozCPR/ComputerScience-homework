a61efb097a0fb48373a99c5909071fd40241c7c8
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2025-03-03 14:03:53 +0100

    Create Viaggio.java

---
+++ Compiti_Java/Compito_Ordinamento/Viaggio.java
@@ -0,0 +1,43 @@
+final public class Viaggio extends Prenotazione{
+    private String mezzo;
+
+
+    public String getMezzo() {
+        return mezzo;
+    }
+
+    public void setMezzo(String mezzo) {
+        if (mezzo.equals("aereo") || mezzo.equals("treno"))
+            this.mezzo = mezzo;
+        else
+            this.mezzo = "aereo"; // di default
+    }
+
+    private String generaCodice(int contatore){
+        return "Viaggio" + contatore;
+    }
+
+    public Viaggio(String prenota, float prezzoBase, String mezzo, int conta) {
+        super(prenota, prezzoBase);
+        super.codiceNumerico += generaCodice(conta);
+        setMezzo(mezzo);
+    }
+
+    @Override
+    public float aggiornaPrezzo() {
+        if (mezzo.equals("aereo"))
+            super.prezzoBase += (float) (prezzoBase * 0.2);
+        else if (mezzo.equals("treno"))
+            super.prezzoBase += (float) (prezzoBase * 0.1);
+
+
+        return super.prezzoBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Viaggio{" +
+                "mezzo='" + mezzo + '\'' +
+                '}';
+    }
+}
