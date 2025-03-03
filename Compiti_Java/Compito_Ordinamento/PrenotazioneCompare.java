24be356bdb02e9c276a6198e957d7339b9f50d09
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2025-03-03 14:15:04 +0100

    Create PrenotazioneCompare.java

---
+++ Compiti_Java/Compito_Ordinamento/PrenotazioneCompare.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class PrenotazioneCompare implements Comparator {
+
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+
+
+}
