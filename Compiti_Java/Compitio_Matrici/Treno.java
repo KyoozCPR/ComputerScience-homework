ac8ecfa30b4738af3f5927c94f050161cb82af71
Kyooz <105115931+KyoozCPR@users.noreply.github.com>
2025-03-13 12:12:59 +0100

    Create Treno.java

---
+++ Compiti_Java/Compitio_Matrici/Treno.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Treno {
+	// formato da un insieme di Vagoni
+	private ArrayList<Vagone> vagoni;
+
+	public ArrayList<Vagone> getVagoni() {
+		return vagoni;
+	}
+
+	public void setVagoni(ArrayList<Vagone> vagoni) {
+		this.vagoni = vagoni;
+	}
+
+	
+	
+}
