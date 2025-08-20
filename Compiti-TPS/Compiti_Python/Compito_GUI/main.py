fd622569e156eb72692ddd469d649436a913a3b9
Diego Computer <cipriettid949@gmail.com>
2025-08-21 01:57:11 +0200

    Changed technology, now using customtkinter for better UI

--- Compiti-TPS/Compiti_Python/Compito_GUI/main.py
+++ Compiti-TPS/Compiti_Python/Compito_GUI/main.py
@@ -6,17 +6,37 @@ import customtkinter as tk
 class Inventario(tk.CTk):
     def __init__(self):
         super().__init__()
+
         tk.set_default_color_theme("dark-blue")
+
+        self.title("Inventario")
         self.geometry("750x500")
-        self.title = tk.CTkLabel(self, text="Benvenuto nell'Inventario")
-        self.title.pack(pady=50)
-        self.frame = tk.CTkFrame(self)
-        self.frame.pack()
-        self.button = tk.CTkButton(self.frame, text="test")
-        self.button2 = tk.CTkButton(self.frame, text="test2")
-        self.button3 = tk.CTkButton(self.frame, text="test3")
-        self.button.grid(column=0, row=0)
-        self.button2.grid(column=1, row=0, padx=20, pady=20)
-        self.button3.grid(column=2, row=0)
+
+
+
+
+        self.greeting = tk.CTkLabel(self, text="Benvenuto nell'inventario")
+        self.greeting.pack()
+        self.greeting_sub = tk.CTkLabel(self, text="hai già un database o vuoi crearlo da 0?")
+        self.greeting_sub.pack()
+        self.framewrapper = tk.CTkFrame(self)
+        self.framewrapper.pack(pady=50)
+        self.button = tk.CTkButton(self.framewrapper, text="Ho un database")
+        self.button2 = tk.CTkButton(self.framewrapper, text="Sì crealo")
+
+        self.button.grid(column=0, row=0, sticky=tk.W)
+        self.button2.grid(column=2, row=0)
+
+
+
+    def cerca_database(self):
+        pass
+
+    def crea_database(self):
+        pass
+
+
+
+
 inventario = Inventario()
 inventario.mainloop()
